41fc1d81667167786348b29f27122e17643fcedc
“DuoLi1999” <liduo1202@gmail.com>
2024-02-05 11:11:36 +0000

    canva_config

---
+++ configs/pixart_config/PixArt_xl2_img512_design.py
@@ -0,0 +1,33 @@
+_base_ = ['../PixArt_xl2_design.py']
+data_root = 'data'
+image_list_txt = ['part0.txt']
+
+data = dict(type='design', root='SA1B', image_list_txt=image_list_txt, transform='default_train', load_vae_feat=False)
+image_size = 512
+
+# model setting
+window_block_indexes=[]
+window_size=0
+use_rel_pos=False
+model = 'PixArt_XL_2'
+fp32_attention = True
+load_from = False
+vae_pretrained = "/home/ld/Project/PixArt-alpha/output/pretrained_models/sd-vae-ft-ema"
+lewei_scale = 1.0
+model_max_length=512
+
+# training setting
+use_fsdp=False   # if use FSDP mode
+num_workers=4
+train_batch_size = 32 # 32
+num_epochs = 200 # 3
+gradient_accumulation_steps = 1
+grad_checkpointing = True
+gradient_clip = 0.01
+optimizer = dict(type='AdamW', lr=2e-5, weight_decay=3e-2, eps=1e-10)
+lr_schedule_args = dict(num_warmup_steps=1000)
+
+eval_sampling_steps = 200
+log_interval = 20
+save_model_epochs=1
+work_dir = 'output/debug'
